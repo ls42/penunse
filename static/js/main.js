168ab84efa557f0e1f6ad7f1dc81b0ceebf66cfb
Stephan Brauer <stephan@ls42.de>
2018-03-27 21:33:14 +0200

    beautify js

--- static/js/main.js
+++ static/js/main.js
@@ -9,37 +9,39 @@ String.prototype.fuzzy = function (s) {
 		n = -1,
 		l
 	s = s.toLowerCase()
-	for (; (l = s[i++]);) if (!~(n = hay.indexOf(l, n + 1))) return false
+	for (;
+		(l = s[i++]);)
+		if (!~(n = hay.indexOf(l, n + 1))) return false
 	return true
 }
 
-let apiBase = "https://penunse.ls42.de/api"
+let apiBase = "http://localhost:4202/api"
+// let apiBase = "https://penunse.ls42.de/api"
 
 // Try a call to the API
-function callAPI () {
+function callAPI() {
 	let request = new Request(`${apiBase}/transaction/read/all`, {
 		headers: new Headers({
 			"X-Clacks-Overhead": "GNU Terry Pratchett"
 		})
 	})
-	fetch(request).then(function(resp) {
-		resp.json().then(function(transactions) {
+	fetch(request).then(function (resp) {
+		resp.json().then(function (transactions) {
 			constructTable()
-		}).catch(function(err) {
+		}).catch(function (err) {
 			console.log(err)
 			console.log("Couldn't convert API data to JSON")
 		})
-	}).catch(function(err) {
+	}).catch(function (err) {
 		console.log("Error calling API")
 	})
 }
 
-function constructTable (transactions) {
+function constructTable(transactions) {
 	let target = document.getElementById("table-grid")
 	let t = document.createElement("table")
 	let thead = document.createElement("thead")
-	let thead_tr = document.createElement("tr")
-	["User", "Amount", "Tags", "Notes"].forEach(function(e) {
+	let thead_tr = document.createElement("tr")["User", "Amount", "Tags", "Notes"].forEach(function (e) {
 		let thead_tr_th = document.createElement("th")
 		thead_tr_th.appendChild(document.createTextNode(e))
 		thead_tr.appendChild(thead_tr_th)
@@ -47,7 +49,7 @@ function constructTable (transactions) {
 	thead.appendChild(thead_tr)
 	t.appendChild(thead)
 	let tbody = document.createElement("tbody")
-	transactions.forEach(function(e) {
+	transactions.forEach(function (e) {
 		let tbody_tr = document.createElement("tr")
 		for (let prop in e) {
 			let tbody_tr_td = document.createElement("td")
