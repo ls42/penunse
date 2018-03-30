81722bc1d6f83a3af683fd771df045e1e89a4f8b
Stephan Brauer <stephan@ls42.de>
2018-03-30 22:58:21 +0200

    So now I fixed access to functions in browser console.

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,12 @@
 import * as sc from "./modules/serverConnector.js"
+import * as dm from "./modules/domManipulator.js";
+
+// Now, usually the app just works fine, but since I'm in development
+// and occasionally I want to call reloadData() from the browser console
+// I apparently have to do this. I have no idea what's going on
+self.sc = sc
+self.dm = dm
+
 
 sc.reloadData()
-
 // setInterval(sc.reloadData, 20000)
