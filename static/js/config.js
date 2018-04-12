74d49367e2ffeb52939832fb501184a12d2815a9
Stephan Brauer <stephan.brauer@olb.de>
2018-04-12 13:02:02 +0200

    Whatever this does

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 export const config = {
-	// apiBase: "http://localhost:4202/api",
-	apiBase: "https://penunse.ls42.de/api",
-	headers: ["Date", "Amount", "Tags", "Note"]
+	apiBase: "http://localhost:4202/api",
+	// apiBase: "https://penunse.ls42.de/api",
+	headers: ["Date", "Amount", "Tags", "Note", "Action"]
 }
