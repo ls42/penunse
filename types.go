f9ab72d229ebd594c2f268784e044b22c67207eb
Stephan Brauer <stebra42@gmail.com>
2018-11-25 00:31:45 +0100

    Make use of softdelete with updated field names

--- types.go
+++ types.go
@@ -9,34 +9,34 @@ import (
 
 // User represents a user of this software
 type User struct {
-	ID      int       `json:"id" gorm:"primary_key"`
-	Login   string    `json:"login"`
-	First   string    `json:"first"`
-	Pass    []byte    `json:"pass"`
-	Created time.Time `json:"created" gorm:"DEFAULT:current_timestamp"`
-	Updated time.Time `json:"updated" gorm:"DEFAULT:current_timestamp"`
-	Deleted time.Time `json:"deleted"`
+	ID        int        `json:"id" gorm:"primary_key"`
+	Login     string     `json:"login"`
+	First     string     `json:"first"`
+	Pass      []byte     `json:"pass"`
+	CreatedAt time.Time  `json:"created" gorm:"DEFAULT:current_timestamp"`
+	UpdatedAt time.Time  `json:"updated" gorm:"DEFAULT:current_timestamp"`
+	DeletedAt *time.Time `json:"deleted"`
 }
 
 // Transaction is an action that affects your depot
 type Transaction struct {
-	ID      int       `json:"id" gorm:"primary_key"`
-	User    int       `json:"user_id"`
-	Amount  float32   `json:"amount"`
-	Tags    []Tag     `json:"tags" gorm:"many2many:transactions_tags;"`
-	Note    string    `json:"note"`
-	Created time.Time `json:"created" gorm:"DEFAULT:current_timestamp"`
-	Updated time.Time `json:"updated" gorm:"DEFAULT:current_timestamp"`
-	Deleted time.Time `json:"deleted"`
+	ID        int        `json:"id" gorm:"primary_key"`
+	User      int        `json:"user_id"`
+	Amount    float32    `json:"amount"`
+	Tags      []Tag      `json:"tags" gorm:"many2many:transactions_tags;"`
+	Note      string     `json:"note"`
+	CreatedAt time.Time  `json:"created" gorm:"DEFAULT:current_timestamp"`
+	UpdatedAt time.Time  `json:"updated" gorm:"DEFAULT:current_timestamp"`
+	DeletedAt *time.Time `json:"deleted"`
 }
 
 // Tag is basically just a string
 type Tag struct {
-	ID      int       `json:"id" gorm:"primary_key"`
-	Name    string    `json:"name"`
-	Created time.Time `json:"created" gorm:"DEFAULT:current_timestamp"`
-	Updated time.Time `json:"updated" gorm:"DEFAULT:current_timestamp"`
-	Deleted time.Time `json:"deleted"`
+	ID        int        `json:"id" gorm:"primary_key"`
+	Name      string     `json:"name"`
+	CreatedAt time.Time  `json:"created" gorm:"DEFAULT:current_timestamp"`
+	UpdatedAt time.Time  `json:"updated" gorm:"DEFAULT:current_timestamp"`
+	DeletedAt *time.Time `json:"deleted"`
 }
 
 // Create saves this Transaction to the database
