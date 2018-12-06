80c7043754b91872fce82c300ca290e0a3df72c9
Stephan Brauer <stebra42@gmail.com>
2018-12-06 22:18:30 +0100

    Working on table.

--- static/app.js
+++ static/app.js
@@ -22,14 +22,14 @@ const conf = {
 
 //
 // Fetch data from API server
-function fetchData(app) {
+function fetchData(vm) {
   let req = new Request(`${conf.apiBaseUrl}/transaction/read`)
   fetch(req)
     .then(resp => {
       resp
         .json()
         .then(ts => {
-          app.transactions = ts
+          vm.transactions = ts
         })
         .catch(err => {
           alert("#1 response not valid JSON")
@@ -40,24 +40,57 @@ function fetchData(app) {
     })
 }
 
+Vue.component("transaction-table-head", {
+  template: `
+    <tr>
+      <th>date</th>
+      <th>amount</th>
+      <th>note</th>
+      <th>name</th>
+      <th>tags</th>
+    </tr>
+  `,
+})
+
 Vue.component("transaction-item", {
-  props: ["trans"],
-  template:
-    "<li>{{ trans.created | dataReadable }} -> {{ trans.amount }} -> <b>{{ trans.note }}</b> -> {{ trans.user_id }} </li>",
+  props: {
+    trans: Array,
+  },
+  filters: {
+    dataReadable: function(date) {
+      return new Date(date).toLocaleDateString("de-DE")
+    },
+    userName: function(user_id) {
+      return user_id ? "Kerstin" : "Stephan"
+    },
+    getTags: function(raw_tags) {
+      let mytags = ""
+      if (raw_tags !== null) {
+        raw_tags.forEach(function(raw_tag) {
+          mytags.concat("asf", `<span class="tag">${raw_tag.name}</span>`)
+        })
+        console.log(mytags)
+        return mytags
+      }
+    },
+  },
+  template: `
+    <tr>
+      <td>{{ trans.created | dataReadable }}</td>
+      <td>{{ trans.amount }},-</td>
+      <td><b>{{ trans.note }}</b></td>
+      <td>{{ trans.user_id | userName }}</td>
+      <td class="tags">{{ trans.tags | getTags }}</td>
+    </tr>`,
 })
 
 // Create the application
 var vm = new Vue({
   el: "#app",
   data: {
-    transactions: null,
+    transactions: [],
   },
-  filters: {
-    dateReadable(date) {
-      return new Date(date).toLocaleDateString("de-DE")
-    },
-  },
-  mounted() {
+  created: function() {
     fetchData(this)
   },
 })
