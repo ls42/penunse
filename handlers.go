2d894dfa9ef717205fccf59b8b482f0fd177ff14
Stephan Brauer <stebra42@gmail.com>
2018-12-14 12:50:03 +0100

    Remove API handlers for now. Will get them back later when auth's done.

--- handlers.go
+++ handlers.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -101,42 +98,5 @@ func saveHandler(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 		}
 		t.Save(db)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
-	}
-}
-
-func apiAllTransactions(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
-	ts := GetTransactions(db)
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(ts)
-}
-
-func apiUpsertTransaction(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
-	reqBody, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	log.Printf("new entry:\t%+v", string(reqBody))
-	if err != nil {
-		http.Error(w, "cannot read data", 400)
-		return
-	}
-	var t Transaction
-	err = json.Unmarshal(reqBody, &t)
-	if err != nil {
-		fmt.Printf("%v", err)
-		http.Error(w, "invalid json", 400)
-		return
-	}
-	t.Create(db)
-}
-
-func apiDeleteTransaction(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
-	transactionID, err := strconv.Atoi(r.URL.Path[len("/api/transaction/delete/"):])
-	if err != nil {
-		http.NotFound(w, r)
-	}
-	var t Transaction
-	if err = db.First(&t, transactionID).Error; err != nil {
-		http.NotFound(w, r)
-	} else {
-		db.Delete(&t)
 	}
 }
--- main.go
+++ main.go
@@ -17,10 +17,6 @@ func main() {
 	mux.HandleFunc("/edit/", makeHandler(editHandler, db))
 	mux.HandleFunc("/save/", makeHandler(saveHandler, db))
 	mux.HandleFunc("/delete/", makeHandler(deleteHandler, db))
-	mux.HandleFunc("/api/transaction/create", makeHandler(apiUpsertTransaction, db))
-	mux.HandleFunc("/api/transaction/read", makeHandler(apiAllTransactions, db))
-	mux.HandleFunc("/api/transaction/update", makeHandler(apiUpsertTransaction, db))
-	mux.HandleFunc("/api/transaction/delete/", makeHandler(apiDeleteTransaction, db))
 
 	log.Printf("listening on port %d\n", p.port)
 	http.ListenAndServe(":"+strconv.Itoa(p.port), mux)
