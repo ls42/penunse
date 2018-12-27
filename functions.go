3322cf6efa3a2d15bb4f4bcbd3ffdfeb70d03404
Stephan Brauer <stebra42@gmail.com>
2018-12-27 21:47:02 +0100

    Start working on recognizing month names.

--- functions.go
+++ functions.go
@@ -9,12 +9,21 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-
 // itob returns an 8-byte big endian representation of v.
 func itob(v int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(v))
 	return b
+}
+
+// inSlice checks if a string is in a slice of strings
+func inSlice(needle string, haystack []string) bool {
+	for _, hay := range haystack {
+		if needle == hay {
+			return true
+		}
+	}
+	return false
 }
 
 // parseFlags defines and then parses all command line flags
--- parseTimesString.go
+++ parseTimesString.go
@@ -3,26 +3,31 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 )
 
 // parseTimeFilterString parses a string and returns a string that is usable in
 // GetTransactionsWithFilters
 func parseTimeFilterString(filter string) string {
-	// months := []string{
-	// 	"jan", "january",
-	// 	"feb", "february",
-	// 	"mar", "march",
-	// 	"apr", "april",
-	// 	"may", "may",
-	// 	"jun", "june",
-	// 	"jul", "july",
-	// 	"aug", "august",
-	// 	"sep", "september",
-	// 	"oct", "october",
-	// 	"nov", "november",
-	// 	"dec", "december",
-	// }
+	fmt.Printf("*%s*", filter)
+
+	filter = strings.TrimSpace(filter)
+	fmt.Printf("*%s*", filter)
+	months := []string{
+		"jan", "january",
+		"feb", "february",
+		"mar", "march",
+		"apr", "april",
+		"may", "may",
+		"jun", "june",
+		"jul", "july",
+		"aug", "august",
+		"sep", "september",
+		"oct", "october",
+		"nov", "november",
+		"dec", "december",
+	}
 	now := time.Now()
 
 	// If time is just a number its an ISO week -> return data from that week
@@ -34,6 +39,11 @@ func parseTimeFilterString(filter string) string {
 		// Display last years data
 		return fmt.Sprintf("strftime('%%W', date, '-1 year') = '%d'", weekOfYear)
 
+	}
+
+	// Check if user input is a month
+	if inSlice(filter, months) {
+		fmt.Println("they've entered a months name O_O")
 	}
 
 	// Default -> return date from current month
