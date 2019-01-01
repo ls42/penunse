79e4da9b9ee4eabbc2c56f551f008f960a5b6eb2
Stephan Brauer <stebra42@gmail.com>
2019-01-01 01:08:03 +0100

    Further work on choosing month. Need to finalize the sql.
    The SQL needs to be correctly build for the SQL date functions.

--- parseTimesString.go
+++ parseTimesString.go
@@ -10,7 +10,7 @@ import (
 // parseTimeFilterString parses a string and returns a string that is usable in
 // GetTransactionsWithFilters
 func parseTimeFilterString(filter string) string {
-	filter = strings.TrimSpace(filter)
+	filter = strings.ToLower(strings.TrimSpace(filter))
 	now := time.Now()
 
 	// If time is just a number its an ISO week
@@ -28,9 +28,11 @@ func parseTimeFilterString(filter string) string {
 	// Check if user input is a month
 	// -> return data from that month (either this or last year's)
 	if month, exists := months[filter]; exists {
-		// 1. Check if month is in the past or future.
-		// 2. If in future, then use data from last year
-		//    Otherwise show data from this year
+		currentMonth := months[strings.ToLower(now.Month().String())]
+		if month <= currentMonth {
+			return "date(date) >= date('now', 'start of month') AND " +
+				"date(date) <= date('now', '+1 month', 'start of month', '-1 day')"
+		}
 		fmt.Printf("would use this string for sql: 2018-%s-*", month)
 	}
 
